13fba9b8207cb2aa6d197b8030b7417e746dccd1
Uduse <wzy950618@gmail.com>
2017-03-21 20:05:23 -0700

    Update script

--- gml_dynamic_completions_and_marks.py
+++ gml_dynamic_completions_and_marks.py
@@ -2,47 +2,48 @@ import sublime
 import sublime_plugin
 import os
 import re
+import json
 
-mark_dirs = ["objects", "rooms", "tilesets", "sprites", "shaders", "sounds"]
-completion_dirs = ["objects", "scripts", "rooms", "tilesets", "sprites", "shaders", "sounds", "timelines"]
+yy_file_regex_pattern = "^.*\.yy$"
 
-def dynamic_completions(search_dirs):
-    completion_list = []
-    var_dict = sublime.active_window().extract_variables()
+def parse_yy_file(path):
+    with open(path, 'r', encoding='utf-8-sig') as f:
+        try:
+            data = json.load(f)
+            model = data['modelName'][2:]
+            name = data['name']
+            if model == "Room":
+                for layer in data['layers']:
+                    yield ("Layer", layer['name'])
+            yield (model, name)
+        except Exception as e:
+            pass
 
-    if 'project_path' in var_dict:
-        path = var_dict['project_path']
-    elif 'folder' in var_dict:
-        path = var_dict['folder']
-    else:
-        return []
-
-    for entry in os.listdir(path):
-        abs_path = os.path.join(path, entry)
-        if os.path.isdir(abs_path):
-            if entry in search_dirs:
-                completion_list.extend(collect_all_names(abs_path))
-    return completion_list
-
-def collect_all_names(root):
-    for path, _, files in os.walk(root):
-        if os.path.isdir(path) and any(re.search("^.*\.(yy|fsh|vsh)$", f) for f in files):
-            yield os.path.basename(path)
+def generate_completions(exclude_folders=["views"]):
+    folders = sublime.active_window().folders()
+    completions = []
+    for folder in folders:
+        for path, _, files in os.walk(folder):
+            if os.path.basename(path) not in exclude_folders:
+                for file in files:
+                    if re.search(yy_file_regex_pattern, file):
+                        completions.extend(parse_yy_file(os.path.join(path, file)))
+    return completions
 
 class GameMakerLanguageCompletions(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         if view.match_selector(locations[0], "source.gml"):
-            return [[c, c] for c in dynamic_completions(completion_dirs)]
-        else:
-            return []
+            return [["%s\t〔%s〕" % (name, model), name] for model, name in generate_completions()]
 
-    def on_modified(self, view):
-        if view.match_selector(locations[0], "source.gml"):
-            view.run_command("mark_dynamic_names")
+    # def on_modified(self, view):
+    #     if re.search(".*\.gml", view.file_name()):
+    #         view.run_command("mark_dynamic_names")
 
-class MarkDynamicNames(sublime_plugin.TextCommand):
-    def run(self, edit):
-        pattern = "(?<![A-Za-z0-9_])" + "|".join(dynamic_completions(mark_dirs)) + "(?![A-Za-z0-9_])"
-        matches = self.view.find_all(pattern)
-        self.view.add_regions("gml_names", matches, scope="source.gml", 
-            flags=sublime.DRAW_NO_OUTLINE | sublime.DRAW_NO_FILL | sublime.DRAW_STIPPLED_UNDERLINE)
+# class MarkDynamicNames(sublime_plugin.TextCommand):
+#     def run(self, edit):
+#         names = [name for _, name in generate_completions(["views", "scripts"])]
+#         print(names)
+#         pattern = "(?<![A-Za-z0-9_])" + "|".join(names) + "(?![A-Za-z0-9_])"
+#         matches = self.view.find_all(pattern)
+#         self.view.add_regions("gml_names", matches, scope="source.gml",
+#                               flags=sublime.DRAW_NO_OUTLINE | sublime.DRAW_NO_FILL | sublime.DRAW_STIPPLED_UNDERLINE)
